fff6c6f2bbcffdea11efa747aeda4bd9a9702e66
Bhavya Boppana <dbopp4@uis.edu>
2024-03-13 22:42:35 -0500

    Revert "Commit"
    This reverts commit 356e8b99eb2958defcff23c875c0ef004a9cae07.
    
    # Conflicts:
    #	src/Repcha.java

---
+++ src/Repcha.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Repcha {
+    public static void main(String argu[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        char[] carray = str.toCharArray();
+        System.out.print("Repetitive Characters in above string are: ");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (carray[i] == carray[j]) {
+                    System.out.print(carray[j] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
