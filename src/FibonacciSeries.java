990584f1013064ffb4fda29a490d96f5ffaaa1dc
Bhavya Boppana <dbopp4@uis.edu>
2024-03-13 21:44:21 -0500

    Commit

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class FibonacciSeries {
+    public void fib(int N){
+        int num1=0,num2=1;
+        for (int i=0;i<N;i++){
+            System.out.print(num1+" ");
+            int num3=num2+num1;
+            num1=num2;
+            num2=num3;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(("Enter N value: "));
+        int N=sc.nextInt();
+        System.out.println("Fibonacci Series:");
+        FibonacciSeries fibonacciSeries=new FibonacciSeries();
+        fibonacciSeries.fib(N);
+    }
 }
