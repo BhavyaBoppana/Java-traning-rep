af7643426828253131004eaa61d53a550de4beb1
Bhavya Boppana <dbopp4@uis.edu>
2024-03-13 22:10:07 -0500

    Commit

---
+++ src/RevString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class RevString {
+    public static void reverseString(String str) {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        reverseString(input);
+    }
+}
